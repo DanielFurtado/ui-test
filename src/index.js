95206b11c5a830d335a1427778860a8cc830bd50
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-01 13:27:36 +0000

    add browser router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </React.StrictMode>,
   rootElement
 );
