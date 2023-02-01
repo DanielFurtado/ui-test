4a7021cba6eab7803c0ab55415971e1d47c8a36a
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-01 13:21:05 +0000

    add the sign up component route

---
+++ src/routes/sign-up/sign-up.component.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+export default SignUp;
