7fb163db45e4f1492d00bbe480d5b5f324c7c6d7
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-01 13:22:14 +0000

    Add routes to App.js and remove api fetch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignUp from './routes/sign-up/sign-up.component';
+
 import './App.css';
 
 const App = () => {
-  const [images, setImages] = useState();
-
-  useEffect(() => {
-    fetch('images?limit=10')
-      .then(res => res.json())
-      .then(data => {
-        console.log('Success:', data);
-        setImages(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, []);
-
   return (
-    <div className='app'>
-      {
-        images && images.map(img => (
-          <div key={img.id} >
-            <img src={`${img.url}.jpg`} alt=''/>
-            <img src={`${img.user.profile_image}.webp`} alt=''/>
-          </div>
-        ))
-      }
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='sign-up' element={<SignUp />} />
+      </Route>
+    </Routes>
   );
 }
 
