deb5f4a808e0802ac123e3deb44e52606ca2f622
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-01 14:33:29 +0000

    update gallery component layout width

--- src/components/gallery/gallery.component.jsx
+++ src/components/gallery/gallery.component.jsx
@@ -5,9 +5,11 @@ import './gallery.styles.scss';
 const Gallery = ({ images }) => {
   return (
     <div className='gallery-container'>
+      <div className='gallery-wrapper'>
       {images && images.map((image) => (
         <CardItem key={image.id} image={image} />
       ))}
+      </div>
     </div>
   );
 };
