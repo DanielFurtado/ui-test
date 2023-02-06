82f0f2115cf579f57bb97aafd72cfc04e36d4a22
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-06 16:32:01 +0000

    add react query to load cached data and renamed gallery route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-import Home from './routes/home/home.component';
+import Gallery from './routes/gallery/gallery.component';
 import Navigation from './routes/navigation/navigation.component';
 import SignUp from './routes/sign-up/sign-up.component';
 
-import './App.css';
+import './App.scss';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='sign-up' element={<SignUp />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Gallery />} />
+          <Route path='sign-up' element={<SignUp />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
