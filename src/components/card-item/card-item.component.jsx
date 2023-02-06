cf26609d42747feedb889859c79b1838a97c18d9
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-06 16:30:31 +0000

    add card modal to card item and update card item styles

--- src/components/card-item/card-item.component.jsx
+++ src/components/card-item/card-item.component.jsx
@@ -1,23 +1,32 @@
+import { Fragment, useState } from 'react';
+import CardModal from '../card-modal/card-modal.component';
+
 import './card-item.styles.scss';
 
 const CardItem = ({ image }) => {
+  const [show, setShow] = useState(false)
   const { url, user } = image;
+  
   return (
-    <div 
-      className='card-container'
-      style={{
-        backgroundImage: `url(${url}.jpg)`,
-      }}
-      >
-        <div className='card-body-container'>
-          <div className='card-info'>
-            <p>{user.name}</p>
+    <Fragment>
+      <div 
+        onClick={() => setShow(true) }
+        className='card-container'
+        style={{ 
+          backgroundImage: `url(${url}.jpg)` 
+        }}
+        data-testid="card-container"
+        >
+          <div className='card-body-container'>
+            <div className='card-info shine'>
+              <p>{user.name}</p>
+              <span className="arrow-right"></span>
+            </div>
           </div>
-        </div>
-    </div>
+      </div>
+      <CardModal onClose={() => setShow(false)} image={image} show={show} />
+    </Fragment>
   );
 };
-
-// <img src={`${user.profile_image}.webp`} alt=''/>
 
 export default CardItem;
