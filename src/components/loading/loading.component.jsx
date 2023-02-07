d1795a93ce2f04ac339f6c26a5c5533618ed37cf
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-07 16:25:23 +0000

    fix loading styles

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -1,13 +1,18 @@
+import { Fragment } from 'react';
+import './loading.styles.scss';
+
 const Loading = () => {
   return (
-    <h3 className='loading'>Loading
-      <div className='lds-ellipsis'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </h3>
+    <Fragment>
+      <h3 className='loading'>Loading
+        <div className='lds-ellipsis'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </h3>
+    </Fragment>
   );
 };
 
